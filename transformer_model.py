12f9816c7b993c313189ca3059acaad384f13179
GhaziGithub <gqj3893@autuni.ac.nz>
2023-09-13 13:45:32 +1200

    changing the encoding method to include attention masks

--- transformer_model.py
+++ transformer_model.py
@@ -1,4 +1,4 @@
-from transformers import BertConfig, BertModel, BertTokenizer, GPT2Tokenizer, GPT2LMHeadModel
+from transformers import GPT2Tokenizer
 from annotate import filter_by_covid, COVID_NAMES
 import torch
 import pandas as pd
@@ -22,44 +22,37 @@ def transform_data(df, output_file_name="output_datasets/finalver1_complete.xlsx
     if 'claims' not in df.columns:
         raise ValueError("The dataframe must contain a 'claims' column.")
 
-    # Initialize the BERT Tokenizer
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     
     # Initialize gpt2 tokenizer
     gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
-    # Initialize GPT model for decoding
-    model = GPT2LMHeadModel.from_pretrained('gpt2')
     
 
 
 
     # Tokenize the 'claims' column
     df['tokenized_claims'] = df['claims'].apply(
-        lambda claims: [tokenizer.tokenize(claim) for claim in claims])
+        lambda claims: [gpt2_tokenizer.tokenize(claim) for claim in claims])
 
     # Encode the tokenized content
 
     df['encoded_claims'] = df['tokenized_claims'].apply(
-        lambda tokenized_content : [gpt2_tokenizer.encode(content) for content in tokenized_content]
+        lambda tokenized_content : [gpt2_tokenizer.encode_plus(content,
+        return_tensors='tf', return_attention_mask=True) for content in tokenized_content]
     )
 
 
+
+
     # decoding the claims
-    df['decoded_claims'] = df['encoded_claims'].apply(lambda encoded_content: [gpt2_tokenizer.decode(encoded,skip_special_token=True) for encoded in encoded_content])
+    # df['decoded_claims'] = df['encoded_claims'].apply(lambda encoded_content: [gpt2_tokenizer.decode(encoded,skip_special_token=True) for encoded in encoded_content])
 
 
     
 
 
-     # Embed the tokenized contents:
 
-    '''
-    df['embedded_claims'] = df['encoded_claims'].apply(
-        lambda ids: [model(id_).last_hidden_state for id_ in ids]
-    )
-    '''
-    # Write the tokenized claims to an Excel file
+    # Write the 2 columns (tokenized and encoded) to an Excel file
     df.to_excel(output_file_name)
 
     return df
@@ -67,6 +60,6 @@ def transform_data(df, output_file_name="output_datasets/finalver1_complete.xlsx
 
 filter_ = filter_by_covid('factver1.xlsx', COVID_NAMES)
 
-transform_data(filter_)
+
 
 
