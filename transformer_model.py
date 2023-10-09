4f8dc4d72f80a4ec846c60ff9c7fcd5427e197bf
GhaziGithub <102706349+GhaziGithub@users.noreply.github.com>
2023-10-09 18:32:19 +1300

    Revert "balancing TF shapes"

--- transformer_model.py
+++ transformer_model.py
@@ -1,23 +1,9 @@
 from transformers import GPT2Tokenizer
 from annotate import filter_by_covid, COVID_NAMES
-import tensorflow as tf
+import torch
+import pandas as pd
 
 
-def encode_and_pad(tokenized_content):
-    max_length = 512
-    input_ids_list = []
-    attention_mask_list = []
-
-    for content in tokenized_content:
-        encoded = gpt2_tokenizer.encode_plus(content, return_attention_mask=True, padding='max_length',
-                                             max_length=max_length, truncation=True)
-        input_ids_list.append(encoded['input_ids'])
-        attention_mask_list.append(encoded['attention_mask'])
-
-    input_ids_tensor = tf.stack(input_ids_list)
-    attention_mask_tensor = tf.stack(attention_mask_list)
-
-    return {'input_ids': input_ids_tensor, 'attention_mask': attention_mask_tensor}
 
 
 def transform_data(df, output_file_name="output_datasets/finalver1_complete.xlsx"):
@@ -36,10 +22,13 @@ def transform_data(df, output_file_name="output_datasets/finalver1_complete.xlsx
     if 'claims' not in df.columns:
         raise ValueError("The dataframe must contain a 'claims' column.")
 
+    
     # Initialize gpt2 tokenizer
-    global gpt2_tokenizer
     gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    gpt2_tokenizer.pad_token = gpt2_tokenizer.eos_token
+
+    
+
+
 
     # Tokenize the 'claims' column
     df['tokenized_claims'] = df['claims'].apply(
@@ -47,10 +36,21 @@ def transform_data(df, output_file_name="output_datasets/finalver1_complete.xlsx
 
     # Encode the tokenized content
 
-    df['encoded_claims'] = df['tokenized_claims'].apply(encode_and_pad)
+    df['encoded_claims'] = df['tokenized_claims'].apply(
+        lambda tokenized_content : [gpt2_tokenizer.encode_plus(content,
+        return_tensors='tf', return_attention_mask=True) for content in tokenized_content]
+    )
 
-    # decoding the claims df['decoded_claims'] = df['encoded_claims'].apply(lambda encoded_content: [
-    # gpt2_tokenizer.decode(encoded,skip_special_token=True) for encoded in encoded_content])
+
+
+
+    # decoding the claims
+    # df['decoded_claims'] = df['encoded_claims'].apply(lambda encoded_content: [gpt2_tokenizer.decode(encoded,skip_special_token=True) for encoded in encoded_content])
+
+
+    
+
+
 
     # Write the 2 columns (tokenized and encoded) to an Excel file
     df.to_excel(output_file_name)
@@ -59,5 +59,6 @@ def transform_data(df, output_file_name="output_datasets/finalver1_complete.xlsx
 
 
 filter_ = filter_by_covid('factver1.xlsx', COVID_NAMES)
-transformed_df = transform_data(filter_, output_file_name="output_datasets/finalver1_complete.xlsx")
+
+# new addition
 
