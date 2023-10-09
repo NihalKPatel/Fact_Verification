54369d9c1d2f489ccaa2d1b9859e335baa01a9a6
GhaziGithub <gqj3893@autuni.ac.nz>
2023-10-09 18:28:30 +1300

    annotate.py file fixed and code refactored

--- annotate.py
+++ annotate.py
@@ -7,71 +7,69 @@ COVID_NAMES = [
 ]
 
 
-def filter_by_covid(file_name, keywords):
-
-    """
+class DataAnnotation:
+    '''
     Clean data and initializing the data in a new column
-    Args:
-        file_name (file): The file name to be read
-        keywords (list): list of strings to filter by
-    Returns:
-        pandas.core.frame.DataFrame: Pandas dataframe output
-    """
+    '''
+    def __init__(self,file_name,keywords):
+        self.file_name = file_name
+        self.keywords = keywords
+        
+        if not isinstance(keywords, list):
+            raise ValueError("keywords param need to be list of strings (a list)")
 
-    # Handling the passed parameters data
+        if not file_name.endswith(".xlsx") and not file_name.endswith(".csv"):
+            raise ValueError("File must be a csv or xlsx.")
+    def clean_data(self):
+        data = pd.read_excel(f"datasets/{self.file_name}")  # Reading the dataset
+        pd_filter = "|".join(self.keywords)
+        es_list = [
+            '"',
+            "*",
+            "/",
+            "(",
+            ")",
+            "\\n",
+            "\\",
+            "\\t",
+            "\\u",
+        ]  # to remove any escape characters and special characters
+        escape_chars = list(map(re.escape, es_list))
 
-    if not isinstance(keywords, list):
-        raise ValueError("keywords param need to be list of strings (a list)")
+        output = data[
+            data["content"].str.contains(pd_filter, case=False)
+            & data["content"].str.endswith("]")
+            | data["headline"].str.contains(pd_filter, na=False)
+        ]  # filter the dataset by the given keywords,filter "out" incomplete data.
 
-    if not file_name.endswith(".xlsx") and not file_name.endswith(".csv"):
-        raise ValueError("File must be a csv or xlsx.")
+        output["content"] = output["content"].replace(
+            escape_chars, "", regex=True
+        )  # removing all escape sequences and special characters
+        output["content"] = output["content"].replace(
+            r'[^\x00-\x7F]', "", regex=True
+            )  # remove all non-ASCII characters
 
-    data = pd.read_excel(f"datasets/{file_name}")  # Reading the dataset
-    pd_filter = "|".join(keywords)
-    es_list = [
-        '"',
-        "*",
-        "/",
-        "(",
-        ")",
-        "\\n",
-        "\\",
-        "\\t",
-        "\\u",
-    ]  # to remove any escape characters and special characters
-    escape_chars = list(map(re.escape, es_list))
+        output.loc[:, "content"] = output["content"].apply(
+            lambda x: x[1:-1].split(",")
+        )  # convert the strings to list of strings
 
-    output = data[
-        data["content"].str.contains(pd_filter, case=False)
-        & data["content"].str.endswith("]")
-        | data["headline"].str.contains(pd_filter, na=False)
-    ]  # filter the dataset by the given keywords,filter "out" incomplete data.
+        output.loc[:, "claims"] = [
+            [
+                e
+                for e in list_
+                if any(keyword for keyword in self.keywords if keyword in e.lower())
+            ]
+            for list_ in output["content"]
+        ]  # adding the filtered claims in a new column "claims"
 
-    output["content"] = output["content"].replace(
-        escape_chars, "", regex=True
-    )  # removing all escape sequences and special characters
-    output["content"] = output["content"].replace(
-        r'[^\x00-\x7F]', "", regex=True
-        )  # remove all non-ASCII characters
+        output.to_excel(
+            "output_datasets/factver1output.xlsx"
+        )  # Write the output in a new file
 
-    output.loc[:, "content"] = output["content"].apply(
-        lambda x: x[1:-1].split(",")
-    )  # convert the strings to list of strings
-
-    output.loc[:, "claims"] = [
-        [
-            e
-            for e in list_
-            if any(keyword for keyword in keywords if keyword in e.lower())
-        ]
-        for list_ in output["content"]
-    ]  # adding the filtered claims in a new column "claims"
-
-    output.to_excel(
-        "output_datasets/factver1output.xlsx"
-    )  # Write the output in a new file
-
-    return output  # Return the output
+        return output  # Return the output
 
 
-filter_by_covid("factver1.xlsx", COVID_NAMES)
+
+filter_ = DataAnnotation("factver1.xlsx", COVID_NAMES)
+filter_.clean_data()
+
